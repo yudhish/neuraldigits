ebe6039d687d51f33d68fe37c3e8e0858f5582a7
tomislav.herman <tomislav.herman@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2008-11-28 20:36:07 +0000

    base for digit

--- trunk/src/hr/fer/zemris/nd/document/DigitField.java
+++ trunk/src/hr/fer/zemris/nd/document/DigitField.java
@@ -1,12 +1,45 @@
-/**
- * 
- */
 package hr.fer.zemris.nd.document;
 
+import java.awt.image.BufferedImage;
+
+
 /**
- * @author goran
+ * 
+ * @author tomislav.herman
  *
  */
 public class DigitField {
+	
+	/**
+	 * Image of the digit, placed in predefined frame size
+	 */
+	private BufferedImage digitImage;	
+	
+	
+	/**
+	 * ctor
+	 * Constructs empty image with predefined size
+	 * @param width width of the image
+	 * @param height height of the image
+	 */
+	public DigitField(int width, int height){
+		digitImage=new BufferedImage(10,10,BufferedImage.TYPE_BYTE_GRAY);		
+	}
+	
+	/**
+	 * ctor
+	 * Construct new DigitFIeld object with predefined image 
+	 * @param image predefined image 
+	 */
+	public DigitField(BufferedImage image){
+		digitImage=image;
+	}
+	
+	/**
+	 * Create a object for every of the 7 segments placed in the digit
+	 */
+	public void createSegments(){
+		
+	}	
 
 }
