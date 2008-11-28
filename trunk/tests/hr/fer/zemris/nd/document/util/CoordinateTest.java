9486e3018817e52686fa799254648beca9f71ee6
tomislav.herman <tomislav.herman@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2008-11-28 20:47:26 +0000

    Coordinate test

---
+++ trunk/tests/hr/fer/zemris/nd/document/util/CoordinateTest.java
@@ -0,0 +1,11 @@
+package hr.fer.zemris.nd.document.util;
+
+public class CoordinateTest {
+	
+	public static void main(String[] args) {
+		Coordinate coord=new Coordinate(10,21);
+		System.out.println(coord);
+	}
+	
+
+}
