762c836c15b8c309a9a2ab69748078632107b175
goranmolnar <goranmolnar@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2008-12-29 06:06:43 +0000

    Histogram framework beta

--- src/hr/fer/zemris/nd/analisys/HistogramMinimaAnaliser.java
+++ src/hr/fer/zemris/nd/analisys/HistogramMinimaAnaliser.java
@@ -4,6 +4,7 @@
 package hr.fer.zemris.nd.analisys;
 
 import hr.fer.zemris.nd.document.util.RectangularArea;
+import hr.fer.zemris.nd.imagelib.Histogram;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
@@ -14,13 +15,13 @@ import java.util.List;
  * @author goran
  *
  */
-public class HistogramSchemeAnaliser implements ISchemeAnaliser{
+public class HistogramMinimaAnaliser implements ISchemeAnaliser{
 
 	BufferedImage image; 
 	WritableRaster raster;
 	
 	
-	public HistogramSchemeAnaliser(BufferedImage image) {
+	public HistogramMinimaAnaliser(BufferedImage image) {
 		this.image = image; 
 		this.raster = image.getRaster();
 	}
@@ -28,30 +29,13 @@ public class HistogramSchemeAnaliser implements ISchemeAnaliser{
 	
 	@Override
 	public List<RectangularArea> getDigitAreas() {
-		double[] xValues = this.getXValues();
+		double[] xValues = Histogram.getXValues(image);
 		System.out.println("X Values: \n");
 		System.out.println(Arrays.toString(xValues));
 		
 		return null;
 	}
 
-
-	private double[] getXValues() {
-		double[] xValues = new double[this.image.getWidth()];
-		for(int i = 0; i < xValues.length; i++) {
-			xValues[i] = getXSum(i);
-		}
-		return xValues;
-	}
-
-
-	private double getXSum(int x) {
-		double sum = 0;
-		for(int i = 0; i < this.raster.getHeight(); i++) {
-			sum += this.raster.getPixel(x, i, new double[1])[0];
-		}
-		
-		return sum;
-	}
+	
 	
 }
---
+++ src/hr/fer/zemris/nd/imagelib/Histogram.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package hr.fer.zemris.nd.imagelib;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author goran
+ *
+ */
+public class Histogram {
+
+	public static double getXSum(BufferedImage image, int x) {
+		if(x < 0 || x > image.getWidth()) {
+			throw new IndexOutOfBoundsException("The x coordinate needs to be " +
+					"within the image bounds. ");
+		}
+		double sum = 0;
+		for(int i = 0; i < image.getRaster().getHeight(); i++) {
+			sum += image.getRaster().getPixel(x, i, new double[1])[0];
+		}
+		return sum;
+	}
+	
+	
+	public double getXAverage(BufferedImage image, int x) {
+		return getXSum(image, x)/image.getHeight();
+	}
+	
+	
+	public static double[] getXValues(BufferedImage image) {
+		double[] xValues = new double[image.getWidth()];
+		for(int i = 0; i < xValues.length; i++) {
+			xValues[i] = getXSum(image, i);
+		}
+		return xValues;
+	}
+	
+	
+	public static double getYSum(BufferedImage image, int y) {
+		if(y < 0 || y > image.getHeight()) {
+			throw new IndexOutOfBoundsException("The x coordinate needs to be " +
+					"within the image bounds. ");
+		}
+		double sum = 0;
+		for(int i = 0; i < image.getRaster().getHeight(); i++) {
+			sum += image.getRaster().getPixel(i, y, new double[1])[0];
+		}
+		return sum;
+	}
+	
+	
+	public double getYAverage(BufferedImage image, int y) {
+		return getXSum(image, y)/image.getWidth();
+	}
+	
+	
+	public static double[] getYValues(BufferedImage image) {
+		double[] yValues = new double[image.getHeight()];
+		for(int i = 0; i < yValues.length; i++) {
+			yValues[i] = getYSum(image, i);
+		}
+		return yValues;
+	}
+	
+	
+
+}
