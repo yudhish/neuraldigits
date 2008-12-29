a3839cd87fa28f5a9f34c70fe5d8953a75018606
goranmolnar <goranmolnar@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2008-12-29 05:32:33 +0000

    Alpha version of the image analysis tools.

--- trunk/src/hr/fer/zemris/nd/document/NumberField.java
+++ trunk/src/hr/fer/zemris/nd/document/NumberField.java
@@ -3,11 +3,24 @@
  */
 package hr.fer.zemris.nd.document;
 
+import hr.fer.zemris.nd.analisys.HistogramSchemeAnaliser;
 import hr.fer.zemris.nd.document.util.RectangularArea;
+import hr.fer.zemris.nd.document.util.ui.BufferedImageDrawer;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.awt.image.renderable.ParameterBlock;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.media.jai.ROI;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 
 /**
  * @author goran
@@ -23,11 +36,14 @@ public class NumberField {
 	public NumberField(BufferedImage numberImage) {
 		this.scan = numberImage;
 		this.digits = new ArrayList<DigitField>();
+		createScheme();
 		createSegments();
 		
 	}
 	
 	
+	
+
 	public NumberField(BufferedImage numberImage, NumberFieldScheme scheme) {
 		this.scan = numberImage;
 		this.scheme = scheme;
@@ -37,6 +53,48 @@ public class NumberField {
 	}
 	
 	
+	private void createScheme() {
+		System.out.println("Initializing scheme.");
+		this.scheme = new NumberFieldScheme(this.scan.getWidth(), this.scan.getHeight());
+		JFrame frame = new JFrame();
+		frame.add(new BufferedImageDrawer(this.scan));
+		frame.setSize(new Dimension(this.scan.getWidth(), this.scan.getHeight()));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		
+		NumberFieldScheme histogramAnalisysScheme = 
+			this.hetHistogramAnalisysScheme();
+		NumberFieldScheme edgeDetectionScheme = getEdgeDetectionScheme();
+		System.out.println("Histogram analisys scheme: \n"+histogramAnalisysScheme);
+		System.out.println();
+		System.out.println(scheme);
+		
+	}
+
+	
+	
+	private NumberFieldScheme getEdgeDetectionScheme() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+	private NumberFieldScheme hetHistogramAnalisysScheme() {
+		HistogramSchemeAnaliser analyser = new HistogramSchemeAnaliser(this.scan);
+		NumberFieldScheme scheme = new NumberFieldScheme(
+				this.scan.getWidth(), this.scan.getHeight());
+		for(RectangularArea area: analyser.getDigitAreas()) {
+			scheme.addInterestArea(area);
+		}
+		
+		return scheme;
+	}
+
+
+
+
 	private void createSegments() {
 		if(this.scheme == null) {
 			getScheme();
