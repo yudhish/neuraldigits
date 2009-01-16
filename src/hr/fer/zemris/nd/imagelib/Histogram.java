f155f2b00ec1cac8e69b65ecc7a32ef71a2f82b2
goranmolnar <goranmolnar@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2009-01-16 21:15:01 +0000

    Histogram drawing finished.

--- src/hr/fer/zemris/nd/imagelib/Histogram.java
+++ src/hr/fer/zemris/nd/imagelib/Histogram.java
@@ -5,6 +5,7 @@ package hr.fer.zemris.nd.imagelib;
 
 import hr.fer.zemris.nd.gui.ImageDisplay;
 
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -45,14 +46,15 @@ public class Histogram {
 	}
 	
 	
-	public static double getYSum(BufferedImage image, int y) {
+	public static int getYSum(BufferedImage image, int y) {
 		if(y < 0 || y > image.getHeight()) {
 			throw new IndexOutOfBoundsException("The x coordinate needs to be " +
 					"within the image bounds. ");
 		}
-		double sum = 0;
+		int sum = 0;
+		int [] pixelValue = new int[3];
 		for(int i = 0; i < image.getRaster().getHeight(); i++) {
-			sum += image.getRaster().getPixel(i, y, new double[1])[0];
+			sum += image.getRaster().getPixel(i, y, pixelValue)[0];
 		}
 		return sum;
 	}
@@ -63,8 +65,8 @@ public class Histogram {
 	}
 	
 	
-	public static double[] getYValues(BufferedImage image) {
-		double[] yValues = new double[image.getHeight()];
+	public static int[] getYValues(BufferedImage image) {
+		int[] yValues = new int[image.getHeight()];
 		for(int i = 0; i < yValues.length; i++) {
 			yValues[i] = getYSum(image, i);
 		}
@@ -73,7 +75,8 @@ public class Histogram {
 	
 	
 	public static void showXHistogram(BufferedImage image) {
-		
+		int[] xValues = getXValues(image);
+		showXHistogram(xValues, image.getType());
 	}
 	
 	
@@ -81,16 +84,14 @@ public class Histogram {
 		int max = getMax(histogramArray);
 		int windowHeight = getXHistogramHeight(max);
 		double scale = max/windowHeight;
-		BufferedImage histogramImage = Histogram.getHistogramGraph(
+		BufferedImage histogramImage = Histogram.getHorizontalHistogramGraph(
 				histogramArray, windowHeight, scale, imageType);
-		ImageDisplay.displayImage(histogramImage);
-		
-		
+		ImageDisplay.displayImage(histogramImage, new Point(150, 300));
 		
 	}
 	
 	
-	private static BufferedImage getHistogramGraph(
+	private static BufferedImage getHorizontalHistogramGraph(
 			int[] histogramArray, int windowHeight, double scale, int imageType) {
 		if(imageType != 10 && imageType != 12) {
 			throw new IllegalArgumentException("The image type is not supported");
@@ -99,16 +100,58 @@ public class Histogram {
 		BufferedImage image;
 		
 		if(imageType == 10) {
-			image = drawType10Image(histogramArray, windowHeight, scale);
+			image = drawType10ImageHorizontal(histogramArray, windowHeight, scale);
 		} else {
-			image = drawType12Image(histogramArray, windowHeight, scale);
+			image = drawType12ImageHorizontal(histogramArray, windowHeight, scale);
 		}
 		
 		return image;
 	}
 
 
-	private static BufferedImage drawType10Image(int[] histogramArray,
+	private static BufferedImage drawType10ImageVertical(int[] histogramArray, int windowWidth, 
+			double scale) {
+		System.out.println("izvrsi se");
+		BufferedImage image = new BufferedImage(
+				windowWidth, histogramArray.length, BufferedImage.TYPE_BYTE_GRAY); 
+		int[] pixelValueBlack = new int[]{0};
+		int[] pixelValueWhite = new int[]{255};
+		
+		for (int i = 0; i < histogramArray.length; i++) {
+			for(int j = 1; j < windowWidth; j++) {
+				if(j < histogramArray[i] * 1/scale) {
+					image.getRaster().setPixel(windowWidth - j, i, pixelValueWhite);
+				} else {
+					image.getRaster().setPixel(windowWidth - j, i, pixelValueBlack);
+				}
+			}
+		}
+		return image;
+	}
+	
+	
+	private static BufferedImage drawType12ImageVertical(int[] histogramArray, int windowWidth, 
+			double scale) {
+		BufferedImage image = new BufferedImage(
+				histogramArray.length, windowWidth, BufferedImage.TYPE_BYTE_GRAY); 
+		int[] pixelValueBlack = new int[]{0};
+		int[] pixelValueWhite = new int[]{255};
+		
+		for (int i = 0; i < histogramArray.length; i++) {
+			for(int j = windowWidth - 1; j >= 0; j--) {
+				if(j < histogramArray[i] * 1/scale) {
+					image.getRaster().setPixel(i, windowWidth - j - 1, pixelValueBlack);
+				} else {
+					image.getRaster().setPixel(i, windowWidth - j - 1, pixelValueWhite);
+				}
+			}
+		}
+		
+		return image;
+	}
+	
+	
+	private static BufferedImage drawType10ImageHorizontal(int[] histogramArray,
 			int windowHeight, double scale) {
 		BufferedImage image = new BufferedImage(
 				histogramArray.length, windowHeight, BufferedImage.TYPE_BYTE_GRAY); 
@@ -129,7 +172,7 @@ public class Histogram {
 	}
 
 
-	private static BufferedImage drawType12Image(int[] histogramArray,
+	private static BufferedImage drawType12ImageHorizontal(int[] histogramArray,
 			int windowHeight, double scale) {
 		BufferedImage image = new BufferedImage(
 				histogramArray.length, windowHeight, BufferedImage.TYPE_BYTE_GRAY); 
@@ -173,6 +216,45 @@ public class Histogram {
 				max = values[i];
 			}
 		}return max;
+	}
+
+
+	public static void showYHistogram(int[] values, int type) {
+		int max = getMax(values);
+		int windowHeight = getYHistogramWidth(max);
+		double scale = max/windowHeight;
+		BufferedImage histogramImage = Histogram.getVerticalHistogramGraph(
+				values, windowHeight, scale, type);
+		ImageDisplay.displayImage(histogramImage, new Point(0, 0));
+		
+	}
+
+
+	private static int getYHistogramWidth(int max) {
+		int windowHeight;
+		if(max < 50) {
+			windowHeight = 50;
+		} else {
+			windowHeight = (int)max;
+		}
+		if(max > 100) {
+			windowHeight = 100;
+		}
+		return windowHeight;
+	}
+
+
+	private static BufferedImage getVerticalHistogramGraph(int[] values,
+			int windowHeight, double scale, int type) {
+		BufferedImage image;
+		
+		if(type == 10) {
+			image = drawType10ImageVertical(values, windowHeight, scale);
+		} else {
+			image = drawType12ImageVertical(values, windowHeight, scale);
+		}
+		
+		return image;
 	}
 
 }
