d0e847c53196375980ba89b828bdc1fd3fcfdd43
tomislav.herman <tomislav.herman@f0b50d5e-bd83-11dd-ad1f-f1f9622dbc98>
2009-01-18 04:42:54 +0000

    ito ay sapat na para sa araw na ito

--- trunk/tests/hr/fer/zemris/nd/document/util/EdgeDigitAnaliserTest.java
+++ trunk/tests/hr/fer/zemris/nd/document/util/EdgeDigitAnaliserTest.java
@@ -1,6 +1,9 @@
 package hr.fer.zemris.nd.document.util;
 
 import hr.fer.zemris.nd.analisys.*;
+import hr.fer.zemris.nd.document.DigitField;
+import hr.fer.zemris.nd.gui.ImageDisplay;
+
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
@@ -18,14 +21,14 @@ public class EdgeDigitAnaliserTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name="prazna";
+		String name="empty";
 		
 		
 		
 		List<BufferedImage> digits=new ArrayList<BufferedImage>();
 		BufferedImage image=null;
 		
-		for(int i=0;i<8;i++){
+		for(int i=0;i<1;i++){
 		///////////////////////////////////////////////////////
 			try {
 				image = ImageIO.read(new File(name+i+".png"));
@@ -43,7 +46,35 @@ public class EdgeDigitAnaliserTest {
 		
 		EdgeDigitAnaliser analis=new EdgeDigitAnaliser(digits);
 		
-		analis.align();
+		IDigitTransformer transformer= analis.getDigitTransformer();
+		
+		try {
+			image = ImageIO.read(new File(name+"0"+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		BufferedImage image2=new BufferedImage(image.getWidth(),image.getHeight(),BufferedImage.TYPE_BYTE_GRAY);
+		image2.getGraphics().drawImage(image, 0, 0, null);
+		
+		
+		DigitField df=transformer.transformImageToDigit(image2);
+		df.showMe();
+		
+		BufferedImage scaled =df.getScaledImage(40, 80);
+		
+		try {
+			ImageIO.write(scaled, "png", new File("d:\\neural\\test3.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		new ImageDisplay(scaled);
+		
+		List<Double> values=df.getSegmentSaturatins();
+		
+		System.out.println(values);
 		
 		
 		
